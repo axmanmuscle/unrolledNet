fixed bug in unrolled net with the order of the wavelet transforms after gradient descent

math_utils.py
@@ -806,8 +806,25 @@ def iwtDaubechies2_np(wt, split = np.array([1])):
 
     return img
 
+def runWavProfile():
+    x = torch.randn(256, 256)
+    split = makeWavSplit(x.shape)
+    split = torch.tensor(split)
 
-if __name__ == "__main__":
+    for _ in range(10):
+        out = wtDaubechies2(x, split)
+
+
+def profile_wavelets():
+    import cProfile
+    import pstats
+
+    cProfile.run("runWavProfile()", "profile.out")
+    p = pstats.Stats("profile.out")
+    p.sort_stats("cumulative").print_stats(20)
+    return 0
+
+def test_wavelets():
     rng = np.random.default_rng(2025)
     for _ in range(20):
         x0 = rng.normal(size=(25, 25))
@@ -823,3 +840,7 @@ if __name__ == "__main__":
     torch_wwt = iwtDaubechies2(torch_ww)
     print(torch.norm(torch_wwt - torch.tensor(wwt)))
     print(torch.norm(torch_ww - torch.tensor(ww)))
+
+if __name__ == "__main__":
+    profile_wavelets()
+    # test_wavelets()

unrolled_net.py
@@ -92,7 +92,7 @@ class final_block_nodc(nn.Module):
         forward method for the final block
         just return the wavelet coefficients to image space
         """
-        x, mask, b = inputs
+        x, mask, b, sMaps = inputs
 
         # wavelet coefficients to image space
         x = math_utils.iwtDaubechies2(torch.squeeze(x), self.wavSplit)
@@ -261,11 +261,12 @@ class unrolled_block(nn.Module):
         with torch.no_grad():
             out = self.grad_desc(x, applyA, b)
 
+            out = self.applyW(out, 'transp')
             if self.dc:
                 out = self.prox(out, mask, b, sMaps)
 
             # wavelets to image space
-            out = self.applyW(out, 'transp')
+            # out = self.applyW(out, 'transp')
 
         out = torch.view_as_real(out)
         n = out.shape[-3]