fixed some masking issues, grad seems to be working

eval_recon_unet.py
@@ -38,6 +38,7 @@ def parse_args():
     parser.add_argument('--checkpoint', type=str, required=True)
     parser.add_argument('--save_dir', type=str, default='./eval_results')
     parser.add_argument('--dc', action='store_true', help="Enforce Data Consistency or not")
+    parser.add_argument('--grad', action='store_true', help="Grad descent steps or not")
     return parser.parse_args()
 
 def main():
@@ -45,13 +46,13 @@ def main():
     os.makedirs(args.save_dir, exist_ok=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    dataset = MRIDataset(kspace_dir=args.data_dir, sens_dir=os.path.join(args.data_dir, './sens_maps'))
+    dataset = MRIDataset(kspace_dir=args.data_dir, sens_dir=os.path.join(args.data_dir, '../subset_sensmaps/sens_maps'))
     loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
     sImg = dataset[0][0].shape[-2:]
     print(sImg)
     wavSplit = torch.tensor(math_utils.makeWavSplit(sImg))
-    model = supervised_net(sImg, device, args.dc)
+    model = supervised_net(sImg, device, args.dc, args.grad)
     model.load_state_dict(torch.load(args.checkpoint, map_location=device))
     model.eval()
     model.to(device)

model_supervised.py
@@ -262,13 +262,42 @@ class grad_desc(nn.Module):
     
     def forward(self, x, sMaps, mask, b):
 
-        Ax = self.applyA(x, sMaps, mask) # [B, H, W, C]
-        resid = Ax - b # [B, H, W, C]
-        gradf = self.applyA(resid, sMaps, mask, 'transp') # [B, H, W]
+        A = lambda x_in: self.applyA(x_in, sMaps, mask)
+        At = lambda x_in: self.applyA(x_in, sMaps, mask, op='transp')
 
-        x_new = x - self.alpha * gradf
+        def obj(xi):
+           return 0.5 * torch.norm(A(xi) - b)**2
+        
+        def grad(xi):
+            xt = A(xi) - b
+            xt = At(xt)
+            return xt
 
-        return x_new
+        # Ax = self.applyA(x, sMaps, mask) # [B, H, W, C]
+        # resid = Ax - b # [B, H, W, C]
+        # gradf = self.applyA(resid, sMaps, mask, 'transp') # [B, H, W]
+
+        # x_new = x - self.alpha * gradf
+
+        gx = grad(x)
+        gxNorm = torch.norm(gx.reshape(-1, 1))**2
+        alpha = 1e-1 # TODO this may need to get changed
+        rho = 0.9
+        c = 0.9
+        max_linesearch_iters = 250
+        obj_x = obj(x)
+
+        linesearch_iter = 0
+        while linesearch_iter < max_linesearch_iters:
+            linesearch_iter += 1
+            xNew = x - alpha*gx
+            obj_xnew = obj(xNew)
+            if obj_xnew < obj_x - alpha * c * gxNorm:
+                break
+            alpha *= rho
+
+        # print(f'grad_descent line search finished after {linesearch_iter} iters at alpha {alpha}')
+        return xNew
 
 class supervised_net(nn.Module):
     def __init__(self, sImg, device, dc=True, grad=False, alpha=1e-3):

run_training_supervised_unet.py
@@ -26,6 +26,7 @@ def parse_args():
     parser.add_argument('--save_dir', type=str, default='./results', help="Directory to save checkpoints and logs")
     parser.add_argument('--epochs', type=int, default=100, help="Number of training epochs")
     parser.add_argument('--dc', action='store_true', help="Enforce Data Consistency or not")
+    parser.add_argument('--grad', action='store_true', help="Do grad descent step/s or not")
     args = parser.parse_args()
     return args
 
@@ -216,6 +217,7 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     logging.info(f"data consistency chosen: {args.dc}")
+    logging.info(f"grad chosen: {args.grad}")
     logging.info(f"device chosen: {device}")
 
     # Define image size
@@ -231,7 +233,7 @@ def main():
     multicoil = True
     # model = build_unet(sImg[-1])
     # model = build_unet_small(sImg[-1])
-    model = supervised_net(sImg, device, dc=dataconsistency)
+    model = supervised_net(sImg, device, dc=dataconsistency, grad=args.grad)
     model = model.to(device)
 
     # Define optimizer