Changed order of operations in the supervised model, fixed a bug related to the normalization of the IFFT when creating images for input/target. Commented out the scaling specifically of the target image

model_supervised.py
@@ -250,6 +250,20 @@ class supervised_net(nn.Module):
       logger = logging.getLogger(__name__)
 
       for iter in range(self.n):
+
+        # step 0: convert to channels and apply unet
+        # in_norm = x.norm(dim=(-2,-1), keepdim=True)
+        x = utils.complex_to_channels(x)
+        if self.share_weights:
+            nn = self.unet
+        else:
+            nn = self.unet_list[iter]
+        
+        x = nn(x)
+        x = utils.channels_to_complex(x)
+        # out_norm = x.norm(dim=(-2,-1), keepdim=True)
+        # x = x / (out_norm + 1e-8) * in_norm
+        
         if self.wav:
             # convert to wavelet coefficients
             # wx = torch.squeeze(x)
@@ -275,19 +289,6 @@ class supervised_net(nn.Module):
             else:
                 logger.info('applying dc zero since eps is empty')
                 x = self.apply_dc_zero(x, mask, b, sMaps)
-
-        # step 3: convert to channels and apply unet
-        in_norm = x.norm(dim=(-2,-1), keepdim=True)
-        x = utils.complex_to_channels(x)
-        if self.share_weights:
-            nn = self.unet
-        else:
-            nn = self.unet_list[iter]
-        
-        x = nn(x)
-        x = utils.channels_to_complex(x)
-        out_norm = x.norm(dim=(-2,-1), keepdim=True)
-        x = x / (out_norm + 1e-8) * in_norm
 
         
       # finally do DC before output

run_training_supervised_unet.py
@@ -323,7 +323,7 @@ def main():
                 kspace_undersampled = kspace * mask_exp
 
             # Initialize input for the model (e.g., zero-filled reconstruction)
-            ks = torch.fft.ifftshift( torch.fft.ifftn(torch.fft.fftshift(kspace_undersampled, dim=[2, 3]), dim = [2, 3]), dim = [2, 3])
+            ks = torch.fft.ifftshift( torch.fft.ifftn(torch.fft.fftshift(kspace_undersampled, dim=[2, 3]), norm='ortho', dim = [2, 3]), dim = [2, 3])
             zf = torch.sqrt((ks.real ** 2 + ks.imag ** 2).sum(dim=1, keepdim=True))  # SoS
 
             ## estim noise
@@ -343,12 +343,12 @@ def main():
             output = model(x_init, mask, kspace_undersampled, sens_maps, eps)  # Output shape: (batch, H, W)
 
             # Compute target image (e.g., inverse Fourier transform of fully-sampled k-space)
-            itarget = torch.fft.ifftshift( torch.fft.ifftn(torch.fft.fftshift(target, dim=[2, 3]), dim = [2, 3]), dim = [2, 3])
+            itarget = torch.fft.ifftshift( torch.fft.ifftn(torch.fft.fftshift(target, dim=[2, 3]), norm='ortho', dim = [2, 3]), dim = [2, 3])
             itarget = torch.permute(itarget, dims=(0, 2, 3, 1))
             itarget1 = itarget * torch.conj(sens_maps)
             target_image = torch.sum(itarget1, dim=-1) # dim -1 is coil dim
 
-            target_image = target_image / torch.max(torch.abs(target_image))
+            # target_image = target_image / torch.max(torch.abs(target_image))
 
             # Normalize both to match scale
             # output = output / output.abs().amax(dim=(-2, -1), keepdim=True)