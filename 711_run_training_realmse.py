df58128be4f016dadf843af3564daf45a14dd449
mcmanus <alex.mcmanus@colorado.edu>
2025-07-11 20:27:50 -0600

    Changing loss calculation to scaling both input and target before

--- 711_run_training_realmse.py
+++ 711_run_training_realmse.py
@@ -86,8 +86,8 @@ def evaluate(model, val_loader, device, mask, criterion):
             target_image = torch.sum(itarget1, dim=-1) # dim 1 is coil dim
 
             # Normalize both to match scale
-            # output = output / output.abs().amax(dim=(-2, -1), keepdim=True)
-            # target_image = target_image / target_image.abs().amax(dim=(-2, -1), keepdim=True)
+            output = output / output.abs().amax(dim=(-2, -1), keepdim=True)
+            target_image = target_image / target_image.abs().amax(dim=(-2, -1), keepdim=True)
             loss = criterion(output.abs(), target_image.abs())
             # loss = criterion(output, target_image)
             val_loss += loss.item()
@@ -331,8 +331,8 @@ def main():
             target_image = torch.sum(itarget1, dim=-1) # dim 1 is coil dim
 
             # Normalize both to match scale
-            # output = output / output.abs().amax(dim=(-2, -1), keepdim=True)
-            # target_image = target_image / target_image.abs().amax(dim=(-2, -1), keepdim=True)
+            output = output / output.abs().amax(dim=(-2, -1), keepdim=True)
+            target_image = target_image / target_image.abs().amax(dim=(-2, -1), keepdim=True)
 
             # Compute loss (MSE between reconstructed and target images)
             loss = criterion(output.abs(), target_image.abs())
